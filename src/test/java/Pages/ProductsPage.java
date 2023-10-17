db534fcb5477f2e8c04eb5747976098abda6e585
Kostantin Bozovic <144264798+kostantin-bozovic@users.noreply.github.com>
2023-10-17 08:01:46 +0200

    Made a methode for Selenium in Java to pick 'n' random products from page and add them to shopping cart

--- src/test/java/Pages/ProductsPage.java
+++ src/test/java/Pages/ProductsPage.java
@@ -1,9 +1,16 @@
 package Pages;
 
 import Base.BaseTest;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class ProductsPage extends BaseTest {
 
@@ -21,7 +28,58 @@ public class ProductsPage extends BaseTest {
     public WebElement menuButton;
     @FindBy(id = "shopping_cart_container")
     public WebElement shoppingCart;
+    @FindBy(className = "inventory_item_name")
+    public List<WebElement> product;
+    @FindBy(css = ".btn.btn_primary.btn_small.btn_inventory") // zbog ovoga pada // prebrzo krece add !!!
+    public WebElement addToCartButton;
+    @FindBy(id = "remove-sauce-labs-backpack")
+    public WebElement removeButton;
+    @FindBy(id = "back-to-products")
+    public WebElement backButton;
 
+
+
+    // Checking if remove button is highlighted !!! // first click on button, then check highlight
+    public boolean buttonIsHighlighted(WebElement button){
+
+        String color = button.getCssValue("color");
+        String borderColor = button.getCssValue("border");
+
+        return color.equals("rgba(226, 35, 26, 1)") && borderColor.equals("0.8px solid rgb(226, 35, 26)");
+    }
+    public void addRandomProducts(int numberOfProducts){
+
+        List<Integer> listOfIndex = new ArrayList<>();
+
+        while (listOfIndex.size() < numberOfProducts){
+
+            int index = randomNumberGenerator(6);
+
+            WebElement product = driver.findElement(By.id("item_"+index+"_title_link"));
+            if (!listOfIndex.contains(index)){
+                listOfIndex.add(index);
+                product.click();
+                clickOnAddToCartButton();
+                clickOnBackButton();
+            }
+        }
+    }
+    public String textWebElement(WebElement element){
+        return element.getText();
+    }
+    public void clickOnBackButton(){
+        backButton.click();
+    }
+    public void clickOnAddToCartButton(){
+        addToCartButton.click();
+    }
+    public void clickOnRemoveButton(){
+        removeButton.click();
+    }
+    public int randomNumberGenerator(int size){ // vraca random index od 0 do product.size
+        Random random = new Random();
+        return random.nextInt(0,size);
+    }
 
     public String getSiteLogoText(){
         return siteLogo.getText();
@@ -37,5 +95,8 @@ public class ProductsPage extends BaseTest {
     }
     public boolean shoppingCartIcon(){
         return shoppingCart.isEnabled() && shoppingCart.isDisplayed();
+    }
+    public int getCartNumber(){
+        return Integer.parseInt(shoppingCart.getText());
     }
 }
--- src/test/java/Tests/HomepageTest.java
+++ src/test/java/Tests/HomepageTest.java
@@ -1,13 +1,20 @@
 package Tests;
 
+import Base.BaseTest;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class HomepageTest extends LoginPageTest {
-    @Test
-    public void verifyIfPageElementsAreVisible(){
+public class HomepageTest extends BaseTest {
 
+    @BeforeMethod
+    public void pageSetUp(){
+        driver.manage().window().maximize();
+        driver.navigate().to(URL);
         validLogin("LoginData");
+    }
+    @Test
+    public void pageElementsArePresent(){
 
         // TESTING URL
         String expectedURL = "https://www.saucedemo.com/inventory.html";
@@ -37,5 +44,39 @@ public class HomepageTest extends LoginPageTest {
 
         // SHOPPING CART - ICON
         Assert.assertTrue(productsPage.shoppingCartIcon());
+    }
+    @Test
+    public void shoppingCartIconShowingCorrectNumber(){
+
+        int number = 2;
+
+        productsPage.addRandomProducts(number);
+
+        int actualNumber = productsPage.getCartNumber();
+        Assert.assertEquals(actualNumber, number);
+
+    }
+    @Test
+    public void UserCanAddProducts() {
+        productsPage.addRandomProducts(5);
+    }
+
+    public void validLogin(String sheetName){
+
+        // Taking from TestData.xlsx file, values for Username and Password
+
+        String validUsername = excelReader.getStringData(sheetName,1,1);
+        String validPassword = excelReader.getStringData(sheetName,1,2);
+
+        login(validUsername,validPassword); // Entering values and clicking Login button
+    }
+    public void login(String username,String password){
+
+        // Enter given values for username and password
+        loginPage.enterUsername(username);
+        loginPage.enterPassword(password);
+
+        // Click on Login button
+        loginPage.clickOnLoginButton();
     }
 }
